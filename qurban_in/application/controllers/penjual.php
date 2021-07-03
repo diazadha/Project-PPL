87e6d393ce08dc6c2680b133ca94c001b74421e2
Haerani6 <86884970+Haerani6@users.noreply.github.com>
2021-07-03 22:23:22 +0700

    menambahkan inputhewan, tambah_hewan, hapus pada controller penjual.php

--- qurban_in/application/controllers/penjual.php
+++ qurban_in/application/controllers/penjual.php
@@ -9,14 +9,54 @@ class penjual extends CI_Controller
         $this->load->view('adminpenjual/register', $data);
     }
 
-    public function inputhewan()
+     public function inputhewan()
     {
         $data['title'] = 'Data Hewan';
+        $data['hewan'] = $this->m_hewan->tampil_data();
         $this->load->view('adminpenjual/templates_adminpenjual/header', $data);
         $this->load->view('adminpenjual/templates_adminpenjual/sidebar', $data);
         $this->load->view('adminpenjual/inputhewan');
         $this->load->view('adminpenjual/templates_adminpenjual/footer');
     }
+
+    public function tambah_hewan()
+    {
+        $nama_hewan          = $this->input->post('nama_hewan');
+        $harga               = $this->input->post('harga');
+        $berat               = $this->input->post('berat');
+        $stok                = $this->input->post('stok');
+        $foto_hewan          = $_FILES['foto_hewan']['name'];
+        $deskripsi           = $this->input->post('deskripsi');
+        $kategori            = $this->input->post('kategori');
+        $kelas               = $this->input->post('kelas');
+        if ($foto_hewan = '') {
+        } else {
+            $config['upload_path']       = './foto_hewan';
+            $config['allowed_types']     = 'jpg|jpeg|png|gif';
+            $config['maintain_ratio']    = TRUE;
+
+            $this->load->library('image_lib', $config);
+            $this->load->library('upload', $config);
+            if (!$this->upload->do_upload('foto_hewan')) {
+                echo "Gambar gagal diupload !";
+            } else {
+                $foto_hewan = $this->upload->data('file_name');
+            }
+            $data = array(
+            'nama_hewan'     => $nama_hewan, 
+            'harga'          => $harga,
+            'berat'          => $berat,
+            'stok'           => $stok,
+            'foto_hewan'     => $foto_hewan,
+            'deskripsi'      => $deskripsi,
+            'kategori'       => $kategori,
+            'kelas'          => $kelas
+        );
+        $this->m_hewan->tambah_hewan($data,'tb_hewan');
+        redirect('penjual/inputhewan');
+        }
+    }
+    
 
     public function detailhewan()
     {
@@ -44,4 +84,15 @@ class penjual extends CI_Controller
         $this->load->view('adminpenjual/detail_pesanan');
         $this->load->view('adminpenjual/templates_adminpenjual/footer');
     }
+    
+    public function hapus($id)
+    {
+        $this->load->model('m_hewan');
+         $detail = $this->m_hewan->dapat_data($id);
+            $path = './foto_hewan/'.$detail->foto_hewan;
+            unlink($path); 
+
+            $this->m_hewan->hapus_data('tb_hewan',array('id_hewan'=>$id));
+            redirect('penjual/inputhewan');
+    } 
 }
