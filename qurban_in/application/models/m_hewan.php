2261fef7e03a144ffdc28a79c5421370c55b1f9b
Haerani6 <86884970+Haerani6@users.noreply.github.com>
2021-07-03 22:33:08 +0700

    Menambahkan file m_hewan.php pada model

---
+++ qurban_in/application/models/m_hewan.php
@@ -0,0 +1,29 @@
+<?php 
+
+class M_hewan extends CI_model
+{
+	public function tambah_hewan($data,$table)
+	{
+        $this->db->insert($table,$data);
+    }
+
+    public function tampil_data(){
+        $result = $this->db->get('tb_hewan');
+        if($result->num_rows() > 0){
+            return $result->result();
+        }else{
+            return FALSE;
+        }
+    }
+
+    public function hapus_data($table,$where)
+    {
+        $this->db->where($where);
+        $this->db->delete($table);
+    }
+
+    public function dapat_data($id_hewan = null){
+        $query = $this->db->get_where('tb_hewan',array('id_hewan'=>$id_hewan))->row();
+        return $query;
+    }
+}
