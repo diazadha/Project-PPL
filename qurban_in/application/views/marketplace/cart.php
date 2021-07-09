f81f779fc03898e5186febe94e109ebbd0b87f3e
Haerani6 <86884970+Haerani6@users.noreply.github.com>
2021-07-09 09:54:47 +0700

    Menambhkan pada Bagian tr dan body pada Subtotal

--- qurban_in/application/views/marketplace/cart.php
+++ qurban_in/application/views/marketplace/cart.php
@@ -7,34 +7,27 @@
                     <div class="table-responsive">
                         <table class="table table-bordered">
                             <thead class="thead-dark">
-                                <tr>
-                                    <th>Product</th>
-                                    <th>Price</th>
+                               <tr>
+                                    <th>No</th>
+                                    <th>Nama Hewan</th>
                                     <th>Quantity</th>
-                                    <th>Total</th>
-                                    <th>Remove</th>
+                                    <th>Harga</th>
+                                    <th>Sub-Total</th>
+                                    
                                 </tr>
                             </thead>
-                            <tbody class="align-middle">
+                            <?php 
+                                $no = 1;
+                                foreach ($this->cart->contents() as $items) : ?>
+                                    <tbody class="align-middle">
                                 <tr>
-                                    <td>
-                                        <div class="img">
-                                            <a href="#"><img src="<?= base_url('assets/') ?>img/sapi_bali.jpg" alt="Image"></a>
-                                            <p>Product Name</p>
-                                        </div>
-                                    </td>
-                                    <td>$99</td>
-                                    <td>
-                                        <div class="qty">
-                                            <button class="btn-minus"><i class="fa fa-minus"></i></button>
-                                            <input type="text" value="1">
-                                            <button class="btn-plus"><i class="fa fa-plus"></i></button>
-                                        </div>
-                                    </td>
-                                    <td>$99</td>
-                                    <td><button><i class="fa fa-trash"></i></button></td>
+                                    <td><?php echo $no++?></td>
+                                    <td><?php echo $items['name'] ?></td>
+                                    <td><?php echo $items['qty'] ?></td>
+                                    <td>Rp. <?php echo number_format($items['price'], 0, ',', '.')  ?></td>
+                                    <td>Rp. <?php echo number_format($items['subtotal'], 0, ',', '.')  ?></td>
                                 </tr>
-                            </tbody>
+								<?php endforeach; ?>
                         </table>
                     </div>
                 </div>
@@ -46,9 +39,8 @@
                             <div class="cart-summary">
                                 <div class="cart-content">
                                     <h1>Cart Summary</h1>
-                                    <p>Sub Total<span>$99</span></p>
-                                    <p>Shipping Cost<span>$1</span></p>
-                                    <h2>Grand Total<span>$100</span></h2>
+                                    <p>Sub Total<span>Rp. <?php echo number_format($this->cart->total(), 0, ',', '.')?></span></p>
+                                    <h2>Grand Total<span>Rp. <?php echo number_format($this->cart->total(), 0, ',', '.')?></span></h2>
                                 </div>
                                 <center>
                                     <div class="col-sm-8">
