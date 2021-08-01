ae62912c414dbaaf73b019037a3c24ed7f6e3bc3
MutiaraLutfiah <86885142+MutiaraLutfiah@users.noreply.github.com>
2021-08-01 17:23:02 +0700

    Menambahkan file update biodata toko pada admin penjual

---
+++ qurban_in/application/views/adminpenjual/biodata_toko.php
@@ -0,0 +1,72 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+    <div class="row">
+        <div class="card shadow col-xl-8 col-lg-7">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold" style="color: #d7552a;">Biodata Toko</h6>
+            </div>
+            <!-- Card Body -->
+            <div class="card-body text-left">
+
+                <form action="<?= base_url('penjual/update_toko/') ?>" method="POST">
+                    <input type="text" name="id_toko" placeholder="Nama Toko" class="form-control" value="<?= $profil['id_toko'] ?>" hidden="" required>
+                    <div class="form-group">
+                        <label>Nama Toko</label>
+                        <input type="text" name="nama_toko" placeholder="Nama Toko" class="form-control" value="<?= $profil['nama_toko'] ?>" required>
+                    </div>
+                    <div class="form-group">
+                        <label>Alamat Toko</label>
+                        <input type="text" name="alamat" placeholder="Alamat Toko" class="form-control" value="<?= $profil['alamat'] ?>" required>
+                        </input>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-sm-6 mb-3 mb-sm-0">
+                            <input type="text" class="form-control form-control-user" name="kota" id="kota" value="<?= $profil['kota'] ?>" placeholder="Kota">
+                        </div>
+                        <div class="col-sm-6">
+                            <input type="text" class="form-control form-control-user" name="provinsi" id="provinsi" value="<?= $profil['provinsi'] ?>" placeholder="Provinsi">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label>No. Telepon</label>
+                        <input type="text" class="form-control form-control-user" id="notlp" name="notlp" value="<?= $profil['notelp'] ?>" placeholder="No. Telepon">
+                    </div>
+                    <button type="submit" class="btn btn-user btn-block" style="background-color: #d7552a; color:white;">
+                        Simpan
+                    </button>
+                </form>
+                <script>
+                    CKEDITOR.replace('deskripsi');
+                </script>
+            </div>
+        </div>
+        <!-- Pie Chart -->
+        <!-- Pie Chart -->
+        <div class="col-xl-4 col-lg-5 col-sm-4">
+            <div class="card shadow mb-4">
+                <!-- Card Header - Dropdown -->
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <h6 class="m-0 font-weight-bold " style="color: #d7552a;">Foto Toko</h6>
+                </div>
+                <!-- Card Body -->
+                <div class="card-body">
+                    <form action="<?= base_url('penjual/update_foto_toko/'); ?>" method="post" enctype="multipart/form-data">
+                        <input type="hidden" class="form-control" id="id_toko" name="id_toko" value="<?= $profil['id_toko']; ?>">
+                        <div class="col 1 mt-2 mb-4" align="center">
+                            <img src=" <?= base_url('uploads/toko/') . $profil['foto_toko']; ?>" class="card-img" alt="...">
+                        </div>
+                        <div class="form-group">
+                            <input type="file" name="foto_toko" class="form-control">
+                        </div>
+                        <button type="submit" class="btn btn-sm" style="width: 100%; background-color: #D7552A; color: white;" aria-haspopup="true" aria-expanded="false">
+                            Update Foto
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+    <!-- /.container-fluid -->
+
+</div>
